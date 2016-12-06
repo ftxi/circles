ef5372e82d21d50dcc44679d1696d61a6a1f0c13
virtualizerocket <1307689960@qq.com>
2016-12-06 21:15:52 +0800

    Dec. 6th, 2016. Basic functions.

--- expend.java
+++ expend.java
@@ -1,7 +1,7 @@
-package circles;
+package test;
 
 import java.awt.*;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class expend extends JFrame
 {
@@ -11,10 +11,15 @@ public class expend extends JFrame
 	
 	private static final long serialVersionUID = 1L;
 	
-	int fps = 40;
-	int speed = 10;
+	int fps = 50;
+	int speed = 120;
 	int unit = 40;
+	int []sizes;
+	boolean []stats;
 	
+	enum block_status_class {_static, _up, _down, _left, _right};
+	block_status_class [][]block_status;
+	int [][]status_number;
 	
 	public static void main(String args[])
 	{
@@ -28,7 +33,20 @@ public class expend extends JFrame
 	
 	expend()
 	{
-		this.setSize(400, 300);
+		int tmp;
+		sizes = new int[speed];
+		stats = new boolean[speed];
+		for(int i = 0; i < speed; i++)
+		{
+			tmp = (int)(Math.cos((double)(i*Math.PI*2/speed))*unit);
+			if(tmp>0)
+				stats[i] = true;
+			else
+				stats[i] = false;
+			sizes[i] = (int)Math.abs(tmp);
+		}
+		
+		this.setSize(1000, 700);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.setVisible(true);
@@ -45,12 +63,59 @@ public class expend extends JFrame
 		status1 = msg;
 	}
 	
+	long tmp2 = 0;
+	
+	/*
+	Image iBuffer;
+	Graphics gBuffer;
+	
+	public void update(Graphics g)
+	{
+		paint(g);
+	}
+	
+	void draw()
+	{
+		if(started)
+		{
+			
+		}
+	}
+	*/
+	
 	public void paint(Graphics g)
 	{
 		started = true;
 		g.clearRect(0, 0, this.getWidth(), this.getHeight());
-		g.setColor(Color.black);
-		g.fillRect(30, 30, 25+(int)(25.0*Math.sin((double)clock.c/speed)), 50);
+		//g.setColor(new Color(255,(int)(128+clock.c%128),0));
+		/*
+		int tmp = Math.abs((int)(200.0*Math.sin((double)clock.c/speed)));
+		
+		g.fillRect(50-tmp, 30, 2*tmp, 50);
+		g.setColor(new Color(0,(int)(128+clock.c%128),tmp));
+		g.fillRect(100, 200-tmp, 600, 2*tmp);
+		*/
+		int tmp = (int) (clock.c%speed);
+		for(int i = 1; i < 10; i++)
+		{
+			for(int j = 1; j < 10; j++)
+			{
+
+				if(stats[tmp])
+					g.setColor(Color.red);
+				else
+					g.setColor(Color.black);
+				//g.fillRect(50*i-sizes[tmp], 50*j, 2*sizes[tmp], 50+unit/2);
+				g.fillRect(60*i+10-sizes[tmp]/2, 60*j+10, sizes[tmp], unit);
+			}
+		}
+		
+		
+		if(clock.c/speed!=tmp2)
+		{
+			tmp2 = clock.c%10;
+			this.showStatus("Running "+tmp2);
+		}
 		
 		g.setFont(new Font("dialog", 10, 10));
 		g.setXORMode(Color.darkGray);
